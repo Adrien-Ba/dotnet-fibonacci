0290d2add4c31f8337d117879ce6939d89165ec5
Adrien-Ba <45963338+Adrien-Ba@users.noreply.github.com>
2022-10-11 09:13:58 +0200

    Create WebApi

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -3,49 +3,36 @@
 using System;
 using System.Diagnostics;
 using System.IO;
-using Demo;
 using Leonardo;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-
 var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");  
 
-IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddEnvironmentVariables()
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
-    .Build();    
-
-var applicationSection = configuration.GetSection("Application");
-var applicationConfig = applicationSection.Get<ApplicationConfig>();
+var stopwatch = new Stopwatch();
+stopwatch.Start();
 
 var services = new ServiceCollection();
 services.AddTransient<FibonacciDataContext>();
 services.AddTransient<Fibonacci>();
 services.AddLogging(configure => configure.AddConsole());
 
-using (var serviceProvider = services.BuildServiceProvider())
-{
-    var logger =serviceProvider.GetService<ILogger<Program>>();
-    
-    logger.LogInformation($"Application Name : {applicationConfig.Name}");
-    logger.LogInformation($"Application Message : {applicationConfig.Message}");
-    
-    var fibonacci = serviceProvider.GetService<Fibonacci>();
-    var results = await fibonacci.Execute(args);
+IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+    .AddEnvironmentVariables().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true).Build();
 
+var applicationSection = configuration.GetSection("Application");
+var applicationConfig = applicationSection.Get<ApplicationConfig>();
+
+using(var serviceProvider = services.BuildServiceProvider()) {
+    var logger = serviceProvider.GetService<ILogger<Program>>();   
+    logger.LogInformation($"Application Name : {applicationConfig.Name}");   
+    logger.LogInformation($"Application Message : {applicationConfig.Message}");   
+    var fibonacci = serviceProvider.GetService<Fibonacci>();   
+    var results = await fibonacci.RunAsync(args);
 }
 
-var loggerFactory = LoggerFactory.Create(builder => {
-    builder.AddFilter("Microsoft", LogLevel.Warning)
-        .AddFilter("System", LogLevel.Warning)
-        .AddFilter("Demo", LogLevel.Debug)
-        .AddConsole();    });
-
-var stopwatch = new Stopwatch();
-
-stopwatch.Start();
 await using var dataContext = new FibonacciDataContext();
 
 var listOfResults = await new Fibonacci(dataContext).RunAsync(args);
@@ -58,11 +45,9 @@ stopwatch.Stop();
 
 Console.WriteLine("time elapsed in seconds : " + stopwatch.Elapsed.Seconds);
 
-namespace Demo
+public class ApplicationConfig
 {
-    public class ApplicationConfig
-    {
-        public String Name { get; set; }
-        public String Message { get; set; }
-    }
+    public string Name { get; set; }
+    public string Message { get; set; }
+        
 }
